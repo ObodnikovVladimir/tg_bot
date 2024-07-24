b88480bf6b93cdf595f4279aca1c5612b760c7e1
ModernMann <105648336+ModernMann@users.noreply.github.com>
2024-07-24 09:03:18 +0300

    Добавлено меню, первые кнопки оформления подписки

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import asyncio
-from aiogram import Dispatcher, Bot
-from aiogram.types import Message
+from aiogram import Dispatcher, Bot, types, F
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import random
 import os
 
 TOKEN = '7387769280:AAHcpy_pXNIHQIYF_cV3B8i-d7Y7GlT8RRY'
@@ -9,11 +12,37 @@ TOKEN = '7387769280:AAHcpy_pXNIHQIYF_cV3B8i-d7Y7GlT8RRY'
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message()
+#Стартовое меню
+@dp.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer('Добрый день')
-    await message.answer('Хотите получить доступ к заблокированным сервисам?')
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Оформить подписку",
+        callback_data="create_subscription")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Узнать статус подписки",
+        callback_data="info_subscription")
+    )
+    await message.answer("Хотите получить доступ к заблокированным сервисам?", reply_markup=builder.as_markup())
+    
+#Ответы из меню
+@dp.callback_query(F.data == "create_subscription")
+async def create_subscription(callback: types.CallbackQuery):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="1 месяц",
+        callback_data="1_month_sub")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="3 месяца",
+        callback_data="3_month_sub")
+    )
+    await callback.message.answer("На сколько хотите оформить подписку?", reply_markup=builder.as_markup())
 
+
+#Запуск бота
 async def main():
     await dp.start_polling(bot)
     
