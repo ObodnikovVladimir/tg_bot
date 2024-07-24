e47bc750bdddc466db961eef30fd1f97ea14b31c
ModernMann <105648336+ModernMann@users.noreply.github.com>
2024-07-24 09:39:40 +0300

    Начало работы с Outline API

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from aiogram.filters import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import random
 import os
+import decouple
+from config import client
+
 
 TOKEN = '7387769280:AAHcpy_pXNIHQIYF_cV3B8i-d7Y7GlT8RRY'
 
@@ -42,6 +45,13 @@ async def create_subscription(callback: types.CallbackQuery):
     await callback.message.answer("На сколько хотите оформить подписку?", reply_markup=builder.as_markup())
 
 
+#Создание новых ключей  доступа через API запрос
+@dp.callback_query(F.data == "1_month_sub")
+async def create_1_month_sub(callback: types.CallbackQuery):
+    key_info = get_key_info("100")
+    await callback.message.answer(f"Вы успешно оформили подписку на 1 месяц{key_info}")
+
+
 #Запуск бота
 async def main():
     await dp.start_polling(bot)
@@ -51,3 +61,17 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Извините, бот временно недоступен!")
+        
+        
+#Функция преобразования Гигабайтов в байты
+def gb_to_bytes(gb: float):
+    bytes_in_gb = 1024 ** 3  
+    return int(gb * bytes_in_gb)
+
+#Получение информации о всех ключах
+def get_keys():
+    return client.get_keys()
+
+#Получение информации по конкретному ключу
+def get_key_info(key_id: str):
+    return client.get_key(key_id)
